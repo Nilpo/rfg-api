32d281ff001a60894789b2009193d096b47627e9
Hayden Bleasel <haydenbleasel@gmail.com>
2014-12-16 15:08:57 +1100

    Asynchronous rewrite, better testing

--- index.js
+++ index.js
@@ -2,93 +2,92 @@
  * rfg-api.js
  * https://github.com/RealFaviconGenerator/rfg-api.js
  *
- * Copyright (c) 2014 Philippe Bernard
+ * Copyright (c) 2014 Philippe Bernard & Hayden Bleasel
  * Licensed under the MIT license.
  */
 
-'use strict';
+/*jslint node:true*/
+module.exports.init = function () {
 
-module.exports.init = function() {
+    'use strict';
 
-  var exports = {};
+    var exports = {},
+        Client = require('node-rest-client').Client,
+        http = require('http'),
+        fs = require('fs'),
+        unzip = require('unzip'),
+        metaparser = require('metaparser'),
+        fstream = require('fstream'),
+        mkdirp = require('mkdirp');
 
-  var Client = require('node-rest-client').Client;
-  var http = require('http');
-  var fs = require('fs');
-  var unzip = require('unzip');
-  var metaparser = require('metaparser');
-  var fstream = require('fstream');
-  var mkdirp = require('mkdirp');
-
-  exports.file_to_base64 = function(file) {
-    return fs.readFileSync(file, {encoding: null}).toString('base64');
-  }
-
-  exports.generate_favicon = function(favicon_gneration_request, dest, callback) {
-    mkdirp.sync(dest);
-
-    var client = new Client();
-    var args = {
-      data: {
-        "favicon_generation": favicon_gneration_request
-      },
-      headers:{"Content-Type": "application/json"}
+    exports.file_to_base64 = function (file, callback) {
+        fs.readFile(file, { encoding: null }, function (error, file) {
+            if (error) throw error;
+            return callback(file.toString('base64'));
+        });
     };
-    client.post("http://realfavicongenerator.net/api/favicon", args, function(data, response) {
-      if (response.statusCode !== 200) {
-        console.log(data);
-        return;
-      }
 
-      var writeStream = fstream.Writer(dest);
-      var parserStream = unzip.Parse();
-      var request = http.get(data.favicon_generation_result.favicon.package_url, function(response) {
-        response.pipe(parserStream).pipe(writeStream);
-      });
-      writeStream.on('close', function() {
-        callback(data.favicon_generation_result);
-      });
-    });
-  }
+    exports.generate_favicon = function (favicon_generation_request, dest, callback) {
+        var client = new Client(),
+            args = {
+                data: {
+                    "favicon_generation": favicon_generation_request
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            };
+        mkdirp(dest, function () {
+            client.post("http://realfavicongenerator.net/api/favicon", args, function (data, response) {
+                if (response.statusCode !== 200) {
+                    throw console.log(data);
+                }
+                var writeStream = fstream.Writer(dest),
+                    parserStream = unzip.Parse(),
+                    request = http.get(data.favicon_generation_result.favicon.package_url, function (response) {
+                        response.pipe(parserStream).pipe(writeStream);
+                    });
+                writeStream.on('close', function () {
+                    callback(data.favicon_generation_result);
+                });
+            });
+        });
+    };
 
-  exports.generate_favicon_markups = function(file, html_code, opts, callback) {
-    var defaultRemove = [
-      'link[rel="shortcut icon"]',
-      'link[rel="icon"]',
-      'link[rel^="apple-touch-icon"]',
-      'link[rel="manifest"]',
-      'link[rel="yandex-tableau-widget"]',
-      'meta[name^="msapplication"]',
-      'meta[name="mobile-web-app-capable"]',
-      'meta[name="theme-color"]',
-      'meta[property="og:image"]'
-    ],
-      add = typeof html_code === 'string' ? [html_code] : html_code,
-      remove = defaultRemove;
+    exports.generate_favicon_markups = function (file, html_code, opts, callback) {
+        var defaultRemove = [
+            'link[rel="shortcut icon"]',
+            'link[rel="icon"]',
+            'link[rel^="apple-touch-icon"]',
+            'link[rel="manifest"]',
+            'link[rel="yandex-tableau-widget"]',
+            'meta[name^="msapplication"]',
+            'meta[name="mobile-web-app-capable"]',
+            'meta[name="theme-color"]',
+            'meta[property="og:image"]'
+        ],
+            add = typeof html_code === 'string' ? [html_code] : html_code,
+            remove = defaultRemove;
 
-    if (opts) {
-      if (opts.add) {
-        var optsAdd = typeof opts.add === 'string' ? [opts.add] : opts.add;
-        add = add.concat(optsAdd);
-      }
-      if (opts.remove) {
-        var optsRemove = typeof opts.remove === 'string' ? [opts.remove] : opts.remove;
-        remove = remove.concat(optsRemove);
-      }
-    }
-
-    metaparser({
-      source: file,
-      add: add,
-      remove: remove,
-      callback: function(error, html) {
-        if (error) {
-          throw error;
+        if (opts) {
+            if (opts.add) {
+                add = add.concat(typeof opts.add === 'string' ? [opts.add] : opts.add);
+            }
+            if (opts.remove) {
+                remove = remove.concat(typeof opts.remove === 'string' ? [opts.remove] : opts.remove);
+            }
         }
-        return callback(html);
-      }
-    });
-  }
 
-  return exports;
-}
+        metaparser({
+            source: file,
+            add: add,
+            remove: remove,
+            callback: function (error, html) {
+                if (error) throw error;
+                return callback(html, add);
+            }
+        });
+    };
+
+    return exports;
+};
--- test.js
+++ test.js
@@ -1,25 +1,33 @@
-var api = require('./index.js').init();
-var fs = require('fs');
+/*jslint node:true*/
 
-var url = 'test/index.html';
-var markup = [
-  '<link rel="icon" type="image/png" href="favicons/favicon-192x192.png" sizes="192x192">',
-  '<link rel="icon" type="image/png" href="favicons/favicon-160x160.png" sizes="160x160">'
-];
-var opts = {
-  add: '<link rel="author" href="humans.txt" />',
-  remove: [
-    'link[href="favicons/favicon-192x192.png"]',
-    'link[href="favicons/favicon-160x160.png"]'
-  ]
-};
+(function () {
 
-api.generate_favicon_markups(url, markup, opts, function (code) {
-  fs.writeFile('test/index-test.html', code, function (err) {
-    if (err) {
-      throw err;
-    } else {
-      console.log('Test complete.')
-    }
-  });
-});
+    'use strict';
+
+    var api = require('./index.js').init(),
+        request = require('./test/request.json'),
+        markup = [
+            '<link rel="icon" type="image/png" href="favicons/favicon-192x192.png" sizes="192x192">',
+            '<link rel="icon" type="image/png" href="favicons/favicon-160x160.png" sizes="160x160">'
+        ],
+        opts = {
+            add: '<link rel="author" href="humans.txt" />',
+            remove: [
+                'link[href="favicons/favicon-192x192.png"]',
+                'link[href="favicons/favicon-160x160.png"]'
+            ]
+        };
+
+    api.file_to_base64('test/sample_picture.png', function (file) {
+        console.log('Generated base64 file: ' + file.substr(0, 10) + '...');
+    });
+
+    api.generate_favicon(request, 'test/favicons/', function (data) {
+        console.log('Generated favicons data: ' + data);
+    });
+
+    api.generate_favicon_markups('test/index.html', markup, opts, function (html, add) {
+        console.log('Generated favicon markup: ' + html.substr(0, 10) + '... with elements added: ' + add.join('').substr(0, 10) + '...');
+    });
+
+}());
